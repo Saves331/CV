4d6c14b905b20edd495d87828d44a69521805497
Saves <martin.l74123@gmail.com>
2025-09-11 15:03:55 +0200

    minor changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,20 +4,28 @@ import React from 'react'
 
 function About() {
 
-  const text = "ABOUT"
+  const about = "ABOUT"
+
 
   return (
-    <div className='h-screen py-[10vh] w-full px-15 relative z-0'>
+    <div className='h-screen py-[10vh] w-full px-15 relative z-0 flex items-center justify-center sm:pt-[20vh] pb-[10vh]'>
 
-        <div className='flex flex-col items-center absolute left-0 top-1/2 -translate-y-1/2 px-2 text-8xl text-white  z-0'>
-  {text.split("").map((char, index) => (
-    <span key={index}>{char}</span>
-  ))}
-</div>
+        <div className=' flex-col items-center absolute left-0 top-1/2 -translate-y-1/2 text-8xl text-white pt-[10vh]  z-0 hidden sm:flex'>
 
-        <div className='text-center max-w-[1800px] mx-auto'>
+     
+          {about.split("").map((char, index) => (
+            <span key={index}>{char}</span>
+          ))}
 
-        <h1 className='text-6xl text-[#F1F5F9] font-bold py-10'>About Me</h1>
+   
+  
+        
+  
+        </div>
+
+        <div className='text-center max-w-[1800px] w-full'>
+
+        <h1 className='text-6xl text-[#F1F5F9] font-bold py-10 sm:hidden'>About Me</h1>
 
 
               <div className='flex gap-4 h-[65vh]'>
--- src/components/Dummy.tsx
+++ src/components/Dummy.tsx
@@ -3,12 +3,26 @@
 function Dummy() {
 
     const boxes = [1,2,3]
+    const skills = "SKILLS"
 
   return (
-    <div className='h-[100vh] w-full bg-amber-600 snap-start pt-[10vh] flex justify-around items-center'>
-        {boxes.map((box) => {
-            return <div key={box} className="w-90 h-120 bg-fuchsia-700 border-2"></div>
-        })}
+    <div className='h-[100vh] w-full bg-amber-600 snap-start pt-[10vh] flex justify-around items-center relative px-15'>
+
+        
+        <div className=' flex-col items-center absolute left-0 top-1/2 -translate-y-1/2 text-8xl text-white  z-0 hidden sm:flex pt-[10vh]'>
+            {skills.split("").map((char, index) => (
+              <span key={index}>{char}</span>
+            ))}
+        </div>
+
+        <div className="flex max-w-[1800px] w-full h-[60vh] gap-4">
+          {boxes.map((box) => {
+            return <div key={box} className="w-1/3 h-[100%] bg-fuchsia-700 border-2"></div>
+          })}
+        </div>
+
+
+        
         
     </div>
   )
